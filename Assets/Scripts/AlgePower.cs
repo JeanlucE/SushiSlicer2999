51679acdcef30afb20f0907b8bb965c6d61e1795
karl.kraus <laqueray@googlemail.com>
2016-06-11 20:41:35 +0200

    Tentakel

---
+++ Assets/Scripts/AlgePower.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+
+public class AlgePower : MonoBehaviour {
+
+    public Vector3 mouthPosition;
+
+    void OnDrawGizmosSelected()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/TentakelAnimation.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TentakelAnimation : MonoBehaviour {
+
+    public Renderer[] renderers;
+    public float swapTime;
+
+    Coroutine cr;
+
+	void Start ()
+    {
+        foreach (Renderer r in renderers)
+        {
+            r.enabled = false;
+        }
+
+        cr = StartCoroutine(Animate());
+	}
+
+    void OnDisable()
+    {
+        StopCoroutine(cr);
+    }
+
+    IEnumerator Animate()
+    {
+        int lastIndex = renderers.Length - 1;
+        int index = 0;
+
+        while (this)
+        {
+            renderers[lastIndex].enabled = false;
+            renderers[index].enabled = true;
+
+            lastIndex = index;
+            index = (index + 1) % renderers.Length;
+
+            yield return new WaitForSeconds(swapTime);
+        }
+    }
+
+}
