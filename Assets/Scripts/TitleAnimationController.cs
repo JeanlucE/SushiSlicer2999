fdbe9007db14370be5f5a628e184924c031f6857
Robert <robert.brand@tum.de>
2016-06-11 21:34:59 +0200

    added not very stylish title screen

---
+++ Assets/Scripts/TitleAnimationController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleAnimationController : MonoBehaviour 
+{
+    public Animator animator;
+
+    public void SetSlice(bool slice)
+    {
+        //animator.SetBool("slice", slice);
+        animator.Play("Slice");
+    }
+}
---
+++ Assets/Scripts/TitleMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TitleMenu : MonoBehaviour 
+{
+    public void OnStartGameButton()
+    {
+        StartCoroutine(StartGame(0.5f));
+    }
+
+    public void OnEndGameButton()
+    {
+        StartCoroutine(QuitGame(0.5f));
+    }
+
+    private IEnumerator QuitGame(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        Application.Quit();
+    }
+
+    private IEnumerator StartGame(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        SceneManager.LoadScene("roberttest");
+    }
+}
