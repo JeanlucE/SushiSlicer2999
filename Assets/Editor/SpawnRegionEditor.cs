a62a4388315b1e9dde0e8e479c115dbd13f832d9
karl.kraus <laqueray@googlemail.com>
2016-06-12 10:40:54 +0200

    spawnregion

---
+++ Assets/Editor/SpawnRegionEditor.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(SpawnRegion))]
+public class SpawnRegionEditor : Editor {
+
+    private SpawnRegion region;
+    private SpawnRegion.SpawnPath nextPath = null;
+
+    private Vector3 point;
+    private List<Vector3> points;
+
+    void OnEnable()
+    {
+        region = this.target as SpawnRegion;
+        points = new List<Vector3>();
+        nextPath = null;
+    }
+
+    Vector2 WindowToScreenPoint(Vector2 position)
+    {
+        return new Vector2(position.x, Screen.height - position.y);
+    }
+
+    void OnSceneGUI()
+    {
+        if (nextPath == null)
+        {
+            Handles.BeginGUI();
+            Rect dropArea = new Rect(Screen.width - 70, 20, 50, 50);
+
+            GUI.Box(dropArea, "Drop List");
+            EnemyDataStore data = RUI.DropArea<EnemyDataStore>(dropArea);
+            if (data)
+            {
+                nextPath = new SpawnRegion.SpawnPath();
+                nextPath.receipt = data;
+                Tools.current = Tool.None;
+
+                points.Clear();
+                SceneView.currentDrawingSceneView.Repaint();
+            }
+
+            Handles.EndGUI();
+        }
+
+        if (nextPath != null)
+        {
+            int control = GUIUtility.GetControlID(new GUIContent("NEXTPATH"), FocusType.Passive);
+            
+            bool finished = false;
+            Camera cam = SceneView.currentDrawingSceneView.camera;
+            Ray r = cam.ScreenPointToRay(WindowToScreenPoint(Event.current.mousePosition));
+
+            Vector3 scenePosition = r.origin - r.direction / r.direction.z * r.origin.z;
+
+            switch (Event.current.type)
+            {
+                case EventType.mouseUp:
+                    finished = true;
+                    GUIUtility.hotControl = 0;
+                    point = scenePosition;
+                    Event.current.Use();
+                    SceneView.currentDrawingSceneView.Repaint();
+                    break;
+                case EventType.mouseDown:
+                    point = scenePosition;
+                    GUIUtility.hotControl = control;
+                    SceneView.currentDrawingSceneView.Repaint();
+                    break;
+                case EventType.mouseDrag:
+                case EventType.mouseMove:
+                    point = scenePosition;
+                    Event.current.Use();
+                    SceneView.currentDrawingSceneView.Repaint();
+                    break;
+                case EventType.Layout:
+                case EventType.Repaint:
+                    Handles.CircleCap(control, point, Quaternion.identity, HandleUtility.GetHandleSize(point) * 0.2f);
+                    break;
+            }
+
+            if (finished)
+            {
+                points.Add(point);
+                if (points.Count == nextPath.receipt.enemyTypes.Count)
+                {
+                    nextPath.points = points.ToArray();
+                    region.paths.Add(nextPath);
+                    nextPath = null;
+                }
+            }
+        }
+
+        foreach (SpawnRegion.SpawnPath path in region.paths)
+        {
+            Handles.Label(path.points[0], path.receipt.name);
+
+            for (int i=0; i<path.points.Length; i++)
+            {
+                path.points[i] = Handles.FreeMoveHandle(path.points[i], Quaternion.identity, HandleUtility.GetHandleSize(path.points[i]) * 0.2f, Vector3.zero, Handles.CircleCap);
+            }
+            Handles.DrawAAPolyLine(path.points);
+        }
+    }
+
+}
---
+++ Assets/SpawnRegion.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnRegion : MonoBehaviour {
+
+    public static SpawnRegion main;
+
+    [System.Serializable]
+	public class SpawnPath
+    {
+        public EnemyDataStore receipt;
+        public Vector3[] points = new Vector3[0];
+
+        public bool intersects(Rect other)
+        {
+            foreach (Vector3 point in points)
+            {
+                if (other.Contains(point))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        GameObject[] instances;
+        public bool stillAlive()
+        {
+            if (instances != null)
+            {
+                foreach (GameObject obj in instances)
+                {
+                    if (obj)
+                    {
+                        return true;
+                    }
+                }
+
+                instances = null;
+            }
+
+            return false;
+        }
+
+        public void Summon(SpawnRegion region)
+        {
+            if (region.paths.Remove(this))
+            {
+                region.spawnedPaths.Add(this);
+
+
+            }
+        }
+    }
+
+    public List<SpawnPath> paths = new List<SpawnPath>();
+    public int initialSpawnCount = 5;
+    
+    private List<SpawnPath> spawnedPaths = new List<SpawnPath>();
+    private Coroutine crSpawn;
+    
+    void Awake()
+    {
+        main = this;
+    }
+
+    void Start()
+    {
+        for (int i=0; i<this.initialSpawnCount && i<paths.Count; i++)
+        {
+            paths[Random.Range(0, paths.Count)].Summon(this);
+        }
+
+        crSpawn = StartCoroutine(EnemySpawner());
+    }
+
+    public static Vector2 GetWorld(Ray r)
+    {
+        return r.origin - r.direction / r.direction.z * r.origin.z;
+    }
+
+    IEnumerator EnemySpawner()
+    {
+        while (this)
+        {
+            spawnedPaths.RemoveAll((path) => {
+                if (path.stillAlive())
+                {
+                    return false;
+                }
+                else
+                {
+                    paths.Add(path);
+                    return false;
+                }
+            });
+
+            Camera cam = Camera.main;
+            Vector2 bottomLeft = GetWorld(cam.ViewportPointToRay(new Vector3(0, 0, 0)));
+            Vector2 topRight = GetWorld(cam.ViewportPointToRay(new Vector3(1, 1, 0)));
+
+            if (paths.Count > 0)
+            {
+                SpawnPath chosenOne = null;
+
+                Rect cameraWorldRect = new Rect(bottomLeft, topRight - bottomLeft);
+                for (int i = 0; i < 5; i++)
+                {
+                    SpawnPath path = paths[Random.Range(0, paths.Count)];
+                    if (path.intersects(cameraWorldRect))
+                    {
+                        chosenOne = path;
+                        break;
+                    }
+                }
+
+                if (chosenOne != null)
+                {
+                    chosenOne.Summon(this);
+                }
+            }
+
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+}
