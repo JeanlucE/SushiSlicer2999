4764492ca69a91cd6d9b20667b871e2aa019c489
Robert <robert.brand@tum.de>
2016-06-12 13:25:46 +0200

    highscore screen and title modifications

---
+++ Assets/Scripts/HighscoreScreen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class HighscoreScreen : MonoBehaviour 
+{
+    public Text scoreDisplay;
+
+    void Start()
+    {
+        float score = ScoreCanvas.lastHighscore;
+        scoreDisplay.text = "" + score;
+
+        StartCoroutine(loadMainMenu(10.0f));
+    }
+
+    private IEnumerator loadMainMenu(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        SceneManager.LoadScene("title");
+    }
+}
