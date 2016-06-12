9ba54397ade4fd413d5489ebe263810f0f75d9cb
Robert <robert.brand@tum.de>
2016-06-12 04:16:52 +0200

    sushi spawning

---
+++ Assets/Scripts/JumpingSushi.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animation))]
+public class JumpingSushi : MonoBehaviour 
+{
+    private Animation animation;
+
+	// Use this for initialization
+	void Start () 
+    {
+        animation = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (!animation.isPlaying) { Destroy(this.gameObject); }
+	}
+}
