e0583e767afa4176405c8fc33c73d557029053b6
karl.kraus <laqueray@googlemail.com>
2016-06-12 14:54:23 +0200

    knock system

--- Assets/Scripts/Knockout.cs
+++ Assets/Scripts/Knockout.cs
@@ -6,11 +6,18 @@ public class Knockout : MonoBehaviour {
     [Range(5f, 120f)]
     public float torpor;
 
+    public GameObject debris;
+
 	void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            PlayerController.main.Knock(torpor, 0.5f * GetComponent<Rigidbody2D>().velocity);
+            Vector3 v = GetComponent<Rigidbody2D>().velocity;
+            PlayerController.main.Knock(torpor, 0.5f * v);
+            if (debris)
+            {
+                GameObject.Instantiate(debris, transform.position, Quaternion.FromToRotation(Vector3.right, -v));
+            }
             Destroy(gameObject);
         }
     }
